05989df25422060d604e2224ce2a049d2fadb488
ProjectLabelIT <projectlabel.it@icloud.com>
2024-07-12 05:33:41 -0700

    Dashboard
    Se agrego un to do list con local storage para que puedan agregar tareas por realizar, como este se encuentra en el local storage, al compartir material compartiran tareas y puede ser una manera de que los supervisores mantengan comunicación en base a tareas por realizar.
    se puede agregar, eliminar y tachar las tareas.

--- admin/inicio.php
+++ admin/inicio.php
@@ -1,3 +1,28 @@
 <?php include("template/cabecera.php"); ?>
-        <img class="dog" src="img/perritoinventario.png">
+<div class="contenedor_tareas">
+        <div class="perfil">
+            <div id="fecha">  </div>
+            <h1>Bienvenido <?php echo $_SESSION['usuario']; ?></h1>
+            <span>Recuerda terminar tus tareas</span>
+        </div>
+
+        <div class="agregar_tarea">
+            <input type="text" id="input" placeholder="Agrega una tarea">
+            <i id="enter" class="fas fa-plus-circle"></i>
+        </div>
+
+        <div class="seccion_tarea">
+            <h3>Tareas pendientes:</h3>
+            <ul id="lista">
+                <!-- EJEMPLO DE COMO SE VERIA CADA OBJETO DE LA LISTA
+                <li>
+                <i class="far fa-circle co" data="realizado" id="0"></i>
+                <p class="text line-through">Hacer la tarea de español</p>
+                <i class="fas fa-trash de" data="eliminado"></i>
+                </li> -->
+            </ul>
+        </div>
+    </div> 
+    <script src="todolist.js"></script>
+
 <?php include("template/pie.php"); ?> 
